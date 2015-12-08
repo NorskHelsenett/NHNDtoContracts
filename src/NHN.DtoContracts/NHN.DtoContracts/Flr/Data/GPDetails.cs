370c39f561b77cae39ac5e88c1d426ae99048a59
bjornm <bjornm@nhn.no>
2015-12-08 08:50:19 +0100

    GPDetails: Remove Status

--- src/NHN.DtoContracts/NHN.DtoContracts/Flr/Data/GPDetails.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Flr/Data/GPDetails.cs
@@ -23,12 +23,6 @@ namespace NHN.DtoContracts.Flr.Data
         public Person Practitioner { get; set; }
 
         /// <summary>
-        /// Statuskode
-        /// </summary>
-        [DataMember]
-        public Code Status { get; set; }
-
-        /// <summary>
         /// Tilknyttede kontrakter. Kun satt ved relevante leseoperasjoner, må være null ellers.
         /// </summary>
         public ICollection<GPOnContractAssociation> Contracts { get; set; }
