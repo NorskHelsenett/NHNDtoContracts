954b7beb2bf0da8b27b2cd3679b0d31130367609
Fredrik Bekkevold <fredrik.bekkevold@nhn.no>
2015-12-11 14:40:47 +0100

    Added SearchParameter and return stream

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Flr/Data/QueryParameters.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using NHN.DtoContracts.Common.en;
+
+namespace NHN.DtoContracts.Flr.Data
+{
+    /// <summary>
+    /// Parametere for export
+    /// </summary>
+    [DataContract(Namespace = FlrXmlNamespace.V1)]
+    public class QueryParameters   
+    {
+        /// <summary>
+        /// Kun pasientlister som tilhører i disse kommunene blir returnert
+        /// Er denne listen tom, returneres alle pasientlister
+        /// </summary>
+        [DataMember]
+        public ICollection<Code> Municipalities { get; set; }
+    }
+}
--- src/NHN.DtoContracts/NHN.DtoContracts/Flr/Service/IFlrExportOperations.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Flr/Service/IFlrExportOperations.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System.IO;
 using System.ServiceModel;
 using NHN.DtoContracts.Common.en;
 using NHN.DtoContracts.Flr.Data;
@@ -12,11 +12,16 @@ namespace NHN.DtoContracts.Flr.Service
     public interface IFlrExportOperations
     {
         /// <summary>
-        /// Henter alle aktive fastlegelister.
+        /// Henter alle GPContracts gruppert pr kontor i en zip-fil
+        /// Bruk søkeparametere for å begrense hva som blir eksportert
         /// </summary>
-        /// <returns></returns>
+        /// <param name="searchParameters">Begrenser innholdet i eksporten</param>
+        /// <returns>Zip file som stream.
+        /// Innholdet i zip filen er en eller flere ICollection av GPContracts serialisert som xml
+        /// Alle GPContracts i en ICollection tilhører samme kontor
+        /// </returns>
         [OperationContract]
         [FaultContract(typeof(GenericFault))]
-        ICollection<GPContract> ExportGPDetails();
+        Stream ExportGPDetails(QueryParameters searchParameters);
     }
 }
