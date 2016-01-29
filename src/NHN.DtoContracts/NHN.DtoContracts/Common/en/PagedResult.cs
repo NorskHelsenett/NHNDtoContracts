ab95803e157230feeaaa8f788344336df2296869
Roy Fatland <roy.fatland@nhn.no>
2016-01-29 08:46:29 +0100

    Added paging to search

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Common/en/PagedResult.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using NHN.DtoContracts.Flr;
+
+namespace NHN.DtoContracts.Common.en
+{
+    /// <summary>
+    /// Generisk paginert resultat.
+    /// </summary>
+    [DataContract(Namespace = FlrXmlNamespace.V1)]
+    public class PagedResult<T>
+    {
+        /// <summary>
+        /// Paginert resultat.
+        /// </summary>
+        [DataMember]
+        public ICollection<T> Results { get; set; }
+
+        /// <summary>
+        /// Total antall resultat.
+        /// </summary>
+        [DataMember]
+        public int Total { get; set; }
+    }
+}
