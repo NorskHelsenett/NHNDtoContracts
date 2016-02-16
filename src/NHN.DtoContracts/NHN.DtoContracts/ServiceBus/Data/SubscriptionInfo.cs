bd009bb42a62d60aeffffa407c0c75f37b10d50a
bjornm <bjornm@nhn.no>
2016-02-16 20:46:44 +0100

    Add ServiceBus alpha methods

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/ServiceBus/Data/SubscriptionInfo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHN.DtoContracts.ServiceBus.Data
+{
+    /// <summary>
+    /// Beskrivelse av et abonnement
+    /// </summary>
+    [DataContract(Namespace = ServiceBusXmlNamespace.V1)]
+    public class SubscriptionInfo
+    {
+        /// <summary>
+        /// Navn på abonnement. 
+        /// </summary>
+        [DataMember]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Path til topic for abonnement.
+        /// </summary>
+        [DataMember]
+        public string TopicPath { get; set; }
+
+        /// <summary>
+        /// This property can be used for the calling system to identify different subscrptions on the same topic.
+        /// </summary>
+        [DataMember]
+        public string UserSystemIdent { get; set;  }
+
+        /// <summary>
+        /// Gir full path til dette abonnement.
+        /// </summary>
+        public string FullPath => $"{TopicPath}/subscriptions/{Name}";
+    }
+}
---
+++ src/NHN.DtoContracts/NHN.DtoContracts/ServiceBus/Service/IServiceBusManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using NHN.DtoContracts.Common.en;
+using NHN.DtoContracts.ServiceBus.Data;
+
+namespace NHN.DtoContracts.ServiceBus.Service
+{
+
+    /// <summary>
+    /// MERK: Dette grensesnittet er i alfa-tilstand og ikke-bakoverkompatible endringer kan skje.
+    /// </summary>
+    [ServiceContract(Namespace = ServiceBusXmlNamespace.V1)]
+    public interface IServiceBusManager
+    {
+        /// <summary>
+        /// Legger til en ny subscription for brukeren som gjør operasjonen, eller henter et eksisterende dersom et abonnement allerde eksisterer.
+        /// </summary>
+        /// <param name="eventSource">Navn på kilde til events. En av "flr", "addresseregister", "hpr", "htk".</param>
+        /// <param name="userSystemIdent">Navn på systemidentifikator for systemet som ønsker abonnementet. Dette er helt brukerspesifikt, men er påkrevd. Bruk bare "Ingen" som konstant parameter dersom det ikke er behov ut ut over ett abonnement på en kø.</param>
+        /// <returns>Informasjon om abonnement</returns>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        SubscriptionInfo AddOrGetSubscription(string eventSource, string userSystemIdent);
+
+        /// <summary>
+        /// Henter informasjon om topic abonnementer på et gitt register.
+        /// </summary>
+        /// <param name="eventSource">Navn på kilde til events. </param>
+        /// <returns>informasjon om abonnement. Merk at hvis ikke noe abonnement finnes, så returneres null.</returns>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        IList<SubscriptionInfo> GetSubscriptions(string eventSource);
+
+        /// <summary>
+        /// Sletter et eksisterende abonnement. Må brukes dersom rettighetene til nåværende bruker har endret seg og man ønsker at dette skal reflekteres i rettigheten på abonnementet.
+        /// </summary>
+        /// <param name="subscriptionInfo"></param>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        void DeleteSubscription(SubscriptionInfo subscriptionInfo);
+    }
+}
