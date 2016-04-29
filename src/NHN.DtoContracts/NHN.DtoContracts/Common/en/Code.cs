40575eba98e0014e360664430dc2dbff8cbf9bd3
bjornm <bjornm@nhn.no>
2016-04-29 09:10:01 +0200

    Code: Add equality checks.

--- src/NHN.DtoContracts/NHN.DtoContracts/Common/en/Code.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Common/en/Code.cs
@@ -82,5 +82,64 @@ namespace NHN.DtoContracts.Common.en
         {
             return $"Code[{SimpleType}({OID}):{CodeValue}{(Active?"":":Inactive")}]";
         }
+
+        /// <summary>
+        /// Equality comparison
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Code c1, Code c2)
+        {
+            if (object.ReferenceEquals(c1, c2))
+                return true;
+            if (object.ReferenceEquals(c1, null) || object.ReferenceEquals(c2, null))
+                return false;
+            return c1.Equals(c2);
+        }
+
+        /// <summary>
+        /// Inequality
+        /// </summary>
+        /// <param name="c1"></param>
+        /// <param name="c2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Code c1, Code c2)
+        {
+            if (object.ReferenceEquals(c1, c2))
+                return false;
+            if (object.ReferenceEquals(c1, null) || object.ReferenceEquals(c2, null))
+                return true;
+            return !c1.Equals(c2);
+        }
+
+        /// <summary>
+        /// Equals.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            var code = obj as Code;
+            if (object.ReferenceEquals(this, code))
+                return true;
+            if (object.ReferenceEquals(code, null))
+                return false;
+            return (code.CodeValue == CodeValue && (CodeValue != null || CodeText == code.CodeText)) &&
+                   ((code.OID > 0 && code.OID == OID) || code.SimpleType == SimpleType) && code.Active == Active;
+        }
+
+        /// <summary>
+        /// GetHashCode.
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            if (OID != 0 && CodeValue != null)
+                return OID.GetHashCode() | CodeValue.GetHashCode();
+            if (OID != 0)
+                return OID.GetHashCode();
+            return SimpleType != null ? SimpleType.GetHashCode() : base.GetHashCode();
+        }
     }
 }
