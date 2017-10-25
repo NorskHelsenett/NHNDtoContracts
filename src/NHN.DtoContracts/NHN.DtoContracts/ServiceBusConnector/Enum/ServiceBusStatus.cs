204a3274dff2cc09352dbf3242d2db1a21cd87da
hyperx\ivark <ivar.katmo@nhn.no>
2017-10-25 14:22:57 +0200

    New service for interaction with service bus. Method for getting message status

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/ServiceBusConnector/Enum/ServiceBusStatus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace NHN.DtoContracts.ServiceBusConnector.Enum
+{
+    /// <summary>
+    /// Status for melding i service bus
+    /// </summary>
+    [DataContract(Namespace = "http://register.nhn.no/ServiceBusConnector")]
+    [Flags]
+    [Serializable]
+    public enum ServiceBusStatus
+    {
+        /// <summary>
+        /// Ukjent
+        /// </summary>
+        [EnumMember]
+        Unknown,
+        /// <summary>
+        /// Meldingen er i kø
+        /// </summary>
+        [EnumMember]
+        InQueue,
+        /// <summary>
+        /// Meldingen er ikke på kø
+        /// </summary>
+        [EnumMember]
+        NotInQueue,
+        /// <summary>
+        /// Melding i deadletter-kø
+        /// </summary>
+        [EnumMember]
+        DeadLetter
+    }
+}
---
+++ src/NHN.DtoContracts/NHN.DtoContracts/ServiceBusConnector/Service/IServicebusService.cs
@@ -0,0 +1,23 @@
+﻿using NHN.DtoContracts.ServiceBusConnector;
+using NHN.DtoContracts.ServiceBusConnector.Enum;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace NHN.DTOContracts.ServiceBusConnector.Service
+{
+    /// <summary>
+    /// Tjeneste for interaksjon med Service Bus 
+    /// </summary>
+    [ServiceContract(Namespace = ServiceBusConnectorNamespace.Name, Name = "IServicebusService")]
+    public interface IServicebusService
+    {
+        /// <summary>
+        /// Henter informasjon om en melding finnes i en av herid's køer
+        /// </summary>
+        /// <param name="herid">HerId</param>
+        /// <param name="messageId">Meldings-id på meldingen det søkes på</param>
+        /// <returns></returns>
+        [OperationContract]
+        Task<ServiceBusStatus> GetMessageStatus(int herid, string messageId);
+    }
+}
