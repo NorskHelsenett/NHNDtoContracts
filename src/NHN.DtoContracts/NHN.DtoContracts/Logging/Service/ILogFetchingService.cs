72d10699517606ddf24da0ad62b584d455f5278b
hildemj <hilde.janie.mjaerfoss@nhn.no>
2017-10-27 13:37:27 +0200

    added documentation and GetFlrRequestTracedLogs method

--- src/NHN.DtoContracts/NHN.DtoContracts/Logging/Service/ILogFetchingService.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Logging/Service/ILogFetchingService.cs
@@ -13,10 +13,26 @@ namespace NHN.DtoContracts.Logging.Service
         /// <summary>
         /// Hente ut loggende forespørsler
         /// </summary>
-        /// <param name="parameter" cref="RequestTrackedLogQueryParameters"></param>
-        /// <returns cref="RequestTrackedLog">RequestTrackedLog med RequestTrackedLog</returns>
+        /// <param name="parameter" cref="RequestTrackedLogQueryParameters">Parametre for å utføre søk på forespørsler</param>
+        /// <returns cref="RequestTrackedLog">Paginert liste over forespørsler. se også <see cref="ResultWithPagination{T}"/></returns>
+        /// <permission>
+        /// Krever en av rollene ADMINISTRATORfor gjeldende register.
+        /// </permission>
         [OperationContract]
         [FaultContract(typeof(GenericFault))]
-        ResultWithPagination<RequestTrackedLog> GetRequestTracedLogs(RequestTrackedLogQueryParameters parameter); 
+        ResultWithPagination<RequestTrackedLog> GetRequestTracedLogs(RequestTrackedLogQueryParameters parameter);
+
+        /// <summary>
+        /// Henter ut loggende forespørseler mot fastlegereigstret (Flr). 
+        /// Dette gjelder da forespørseler til Flr tjenestene FlrReadOperationService og FlrExportService. 
+        /// </summary>
+        /// <param name="parameter" cref="RequestTrackedLogQueryParameters">Parametre for å utføre søk på forespørsler. Parameteret ServiceName vil her være begrenset til Flr relaterte tjenester.</param>
+        /// <returns cref="RequestTrackedLog">Paginert liste over forespørsler. se også <see cref="ResultWithPagination{T}"/></returns>
+        /// <permission>
+        /// Krever en av rollene ADMINISTRATOR eller FLR_READ_REQUEST_TRACKED_LOG for gjeldende register.
+        /// </permission>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        ResultWithPagination<RequestTrackedLog> GetFlrRequestTracedLogs(RequestTrackedLogQueryParameters parameter);
     }
 }
