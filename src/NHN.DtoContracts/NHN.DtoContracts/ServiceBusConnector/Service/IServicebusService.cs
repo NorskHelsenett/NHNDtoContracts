3cbb7f95a1c55fe30c6aa8dfdabf9b6749cf9df9
hyperx\ivark <ivar.katmo@nhn.no>
2017-11-16 15:27:32 +0100

    Updated documentation

--- src/NHN.DtoContracts/NHN.DtoContracts/ServiceBusConnector/Service/IServicebusService.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/ServiceBusConnector/Service/IServicebusService.cs
@@ -13,11 +13,18 @@ namespace NHN.DTOContracts.ServiceBusConnector.Service
     public interface IServicebusService
     {
         /// <summary>
-        /// Henter informasjon om en melding finnes i en av herid's køer
+        /// Henter informasjon om en meldings status på en kø på service bus. Hvor det søkes etter meldingen avhenger av køene som er koblet til virksomheten i Adresseregisteret.
         /// </summary>
-        /// <param name="herid">HerId</param>
+        /// <param name="herid">Virksomhetens herId</param>
         /// <param name="messageId">Meldings-id på meldingen det søkes på</param>
-        /// <returns></returns>
+        /// <returns>Status for meldingen</returns>
+        /// <example>
+        /// <code>
+        /// serviceBusService.GetMessageStatus(98557, a0424e93-dfce-4497-9ae0-083aab52f86a);
+        /// </code>
+        /// </example>
+        /// <exception cref="FaultException">Kastes om oppslag mot servicebus feiler for en av </exception>
+        /// <permission>Krever rollen ServiceBus-bruker</permission>
         [OperationContract]
         [FaultContract(typeof(GenericFault))]
         Task<ServiceBusStatus> GetMessageStatus(int herid, string messageId);
