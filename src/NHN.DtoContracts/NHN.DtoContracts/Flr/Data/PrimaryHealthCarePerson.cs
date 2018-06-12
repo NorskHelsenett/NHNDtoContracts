c96acde386ce6e6e540504ebb086999f6b79cdd3
John Arne Øye <johnarneoye@gmail.com>
2018-06-12 10:45:33 +0200

    Add new dto contracts for primary healthcare team.

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Flr/Data/PrimaryHealthCarePerson.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.Serialization;
+using NHN.DtoContracts.Common.en;
+
+namespace NHN.DtoContracts.Flr.Data
+{
+    /// <summary>
+    /// Basisdata for en person.
+    /// </summary>
+    [DataContract(Namespace = Namespaces.FlrV1)]
+    [Serializable]
+
+    public class PrimaryHealthCarePerson
+    {
+        /// <summary>
+        /// Identifikator for primærhelseteamperson.
+        /// </summary>
+        [DataMember]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// HPR-nummer for personen.
+        /// </summary>
+        [DataMember]
+        public int HprNumber { get; set; }
+
+        /// <summary>
+        /// Code som beskriver rollen til en person i kodeverk Helsepersonell (OID=9060).
+        /// </summary>
+        [DataMember]
+        public Code Role { get; set; }
+
+        /// <summary>
+        /// Beskriver hvorvidt personen er en vikar eller ikke.
+        /// </summary>
+        [DataMember]
+        public bool IsSubstitute { get; set; }
+
+        /// <summary>
+        /// Andel stilling til personen i prosent.
+        /// </summary>
+        [DataMember]
+        public int WorkingPercentage { get; set; }
+
+        /// <summary>
+        /// Perioden personen er medlem i primærhelseteam.
+        /// </summary>
+        [DataMember]
+        public Period MembershipPeriod { get; set; }
+
+        /// <summary>
+        /// Opprettelsesdato for objektet.
+        /// </summary>
+        [DataMember]
+        public DateTime CreatedOn { get; set; }
+
+        /// <summary>
+        /// Dato for sist oppdatering av objektet.
+        /// </summary>
+        [DataMember]
+        public DateTime UpdatedOn { get; set; }
+    }
+}
---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Flr/Data/PrimaryHealthCareTeamEdit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace NHN.DtoContracts.Flr.Data
+{
+    /// <summary>
+    /// Basisdata for en person.
+    /// </summary>
+    [DataContract(Namespace = Namespaces.FlrV1)]
+    [Serializable]
+
+    public class PrimaryHealthCareTeamEdit
+    {
+        /// <summary>
+        /// Identifikator for primærhelseteam.
+        /// </summary>
+        [DataMember]
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Organisasjonsnummer for tilhørende organisasjon.
+        /// </summary>
+        [DataMember]
+        public int OrganizationNumber { get; set; }
+
+        /// <summary>
+        /// Liste over all helsepersonell knyttet til primærhelseteamet.
+        /// </summary>
+        [DataMember]
+        public List<PrimaryHealthCarePersonEdit> PrimaryHealthCarePeople { get; set; }
+
+        /// <summary>
+        /// Liste over kontraktene til primærhelseteamet.
+        /// </summary>
+        [DataMember]
+        public List<PrimaryHealthCareContractEdit> PrimaryHealthCareContracts { get; set; }
+
+        /// <summary>
+        /// HPR-nummer til lederen for primærhelseteamet.
+        /// </summary>
+        [DataMember]
+        public int PrimaryHealthCareLeaderHprNumber { get; set; }
+
+        /// <summary>
+        /// Versjon fra FLO.
+        /// </summary>
+        [DataMember]
+        public int Version { get; set; }
+    }
+}
