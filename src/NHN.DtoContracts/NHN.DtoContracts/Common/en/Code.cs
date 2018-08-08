145fdbd072dcb2a951a535f1ba3a2678466cd080
Andreas Gullberg Larsen <Andreas.Gullberg.Larsen@nhn.no>
2018-08-08 15:31:00 +0200

    Remove unused usings

--- src/NHN.DtoContracts/NHN.DtoContracts/Common/en/Code.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Common/en/Code.cs
@@ -1,10 +1,5 @@
-﻿using NHN.DtoContracts.Common;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NHN.DtoContracts.Common.en
 {
