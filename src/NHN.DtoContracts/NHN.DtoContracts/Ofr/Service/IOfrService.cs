8289f70c7ad34e0358e5c57e49060ef8bdea883c
oeyaaroe <oyvind.aaroe@inmeta.no>
2018-08-13 13:19:48 +0200

    Test commit
    modified:   src/NHN.DtoContracts/NHN.DtoContracts/Ofr/Service/IOfrService.cs

--- src/NHN.DtoContracts/NHN.DtoContracts/Ofr/Service/IOfrService.cs
+++ src/NHN.DtoContracts/NHN.DtoContracts/Ofr/Service/IOfrService.cs
@@ -78,8 +78,10 @@ namespace NHN.DtoContracts.Ofr.Service
         [OperationContract]
         [FaultContract(typeof(GenericFault))]
         HealthRegister UpdateHealthRegister(HealthRegister healthRegister);
-        ///Test
-        
+        ///
+        /// 
+
+
         /// <summary>
         /// Sletter en helseregisteroppføring fra registeret.
         /// </summary>
