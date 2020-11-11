a6199dcbcf7956cc8d32de60251c7cdbe24e643f
Erling Børresen <erling.borresen@gmail.com>
2020-11-11 15:16:11 +0100

    New public API for KRK service

---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Krk/Data/KommuneoverlegeInfo.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace NHN.DtoContracts.Krk.Data
+{
+    /// <summary>
+    /// Helseregister objekt for å legge til/hente ut opplysninger om en helseregisteroppføring i kontaktregister for kommuneoverleger (KRK)
+    /// </summary>
+    [DataContract(Namespace = Namespaces.KrkV1)]
+    public class KommuneoverlegeInfo
+    {
+
+        /// <summary>
+        /// Organisasjonen kommuneoverlegetjenesten er registrert på
+        /// </summary>
+        //TODO: Trenger vi denne? Kun ved skriving?
+        [DataMember]
+        public string OrganizationNumber { get; set; }
+
+        /// <summary>
+        /// Områdene som dekkes av denne kommuneoverlegeinformasjonen.
+        /// Kan være kommuner og bydeler
+        /// </summary>
+        [DataMember]
+        public ICollection<string> DistrictIds { get; set; }
+
+        /// <summary>
+        /// Liste over tilknyttede helsepersonells HPR nummer
+        /// </summary>
+        [DataMember, Required]
+        public ICollection<string> RegisteredHprIds { get; set; }
+
+        /// <summary>
+        /// Telefonnummer for kommuneoverlegen
+        /// </summary>
+        [DataMember]
+        public string PhoneNumber { get; set; }
+
+        /// <summary>
+        /// Epost for kommuneoverlegen
+        /// </summary>
+        [DataMember]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Nødnettnummer for kommuneoverlegen
+        /// </summary>
+        [DataMember]
+        public string PublicSafetyNetId { get; set; }
+    }
+}
---
+++ src/NHN.DtoContracts/NHN.DtoContracts/Krk/Service/IKrkService.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using NHN.DtoContracts.Common.en;
+using NHN.DtoContracts.Krk.Data;
+
+namespace NHN.DtoContracts.Krk.Service
+{
+    /// <summary>
+    /// Tjeneste for å hente ut og legge til informasjon i kontaktregister for kommuneoverleger (KRK)
+    /// </summary>
+    [ServiceContract(Namespace = Namespaces.KrkV1,
+        Name = "IKrkService")]
+    public interface IKrkService
+    {
+        //TODO. Dokumenter hvilke roller som kreves
+
+        /// <summary>
+        /// Henter ut alle kommuner og bydeler der denne personen er registrert som deltaker i kommuneoverlegetjenesten
+        /// </summary>
+        /// <param name="HprId">Helsepersonellnummeret til den aktuelle personen</param>
+        /// <returns>Liste over kommuner og bydeler</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <example>
+        /// <code>
+        /// var districts = krkService.GetKommunerAndBydelForHPR(hprId);
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// ##### Krever en av rollene
+        /// *
+        /// </remarks>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        ICollection<string> GetKommunerAndBydelForHPR(string hprId);
+
+        /// <summary>
+        /// Henter ut alle helsepersonell som er registrert som deltaker i kommuneoverlegetjenesten for denne kommunen eller bydelen
+        /// </summary>
+        /// <param name="districtID">IDen til en kommune eller bydel (f.eks. 030112) </param>
+        /// <returns>Liste over helsepersonellnumre</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <example>
+        /// <code>
+        /// var hprNumbers = krkService.GetHPRForKommuneOrBydel(districtID);
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// ##### Krever en av rollene
+        /// *
+        /// </remarks>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        ICollection<string> GetHPRForKommuneOrBydel(string districtID);
+
+
+        //TODO: Hva skjer dersom man gir inn IDen til oslo, skal alle bydeler komme med? Dokumenterer som om det stemmer
+        /// <summary>
+        /// Henter ut informasjon om kommuneoverlegetjenesten for denne kommunen eller bydelen.
+        ///
+        /// For kommuner uten egen kommuneoverlegetjeneste på bydelsnivå vil denne listen inneholde ett innslag.
+        ///
+        /// For kommuneoverlegetjenester som leveres av en kommune i et interkommunalt samarbeid,
+        /// vil denne metoden gi samme resultat om du gir inn et kommunenummer i dekningsområdet til tjenesten.
+        /// </summary>
+        /// <param name="districtID">IDen til en kommune eller bydel (f.eks. 030112) </param>
+        /// <returns>Liste med informasjon om kommuneoverlegetjenesten</returns>
+        /// <exception cref="ArgumentException"></exception>
+        /// <example>
+        /// <code>
+        /// var infoObjects = krkService.GetKommuneOverlegeInfos(districtID);
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// ##### Krever en av rollene
+        /// *
+        /// </remarks>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        ICollection<KommuneoverlegeInfo> GetKommuneOverlegeInfos(string districtID);
+
+
+        /// <summary>
+        /// Utfører et søk på kommuneoverlegetjenester med gitte parametre.
+        /// </summary>
+        /// <param name="searchTerm">Tekst for å utføre søk.</param>
+        /// <returns>Resultat med liste over kommuneoverlegeinfo.</returns>
+        /// <example>
+        /// <code>
+        /// var searchResult = krkService.SearchKommuneOverlegeInfo(searchTerm);
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// ##### Krever en av rollene
+        /// *
+        /// </remarks>
+        [OperationContract]
+        [FaultContract(typeof(GenericFault))]
+        ICollection<KommuneoverlegeInfo> SearchKommuneOverlegeInfo(string searchTerm);
+    }
+}
